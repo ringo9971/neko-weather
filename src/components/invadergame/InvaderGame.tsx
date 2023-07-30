4ec1e33a17933c82d286b6f5fbe10beabd0b8551
ringo9971 <ab7usa@gmail.com>
2023-07-30 12:01:54 +0900

    :sparkles: Invader に動きを追加

--- src/components/invadergame/InvaderGame.tsx
+++ src/components/invadergame/InvaderGame.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { memo, useEffect, useState } from 'react';
+import { memo, useCallback, useEffect, useState } from 'react';
 
+import { Direction } from '../../enums';
 import { InvaderGameContext } from '../../lib/contexts';
 import Invaders from './Invaders';
 import { InvaderModel } from './Invaders';
@@ -25,12 +26,23 @@ export interface CellGrid {
   };
 }
 
+interface MoveCondition {
+  id: number;
+  dir: Direction;
+  change: boolean;
+}
+
 const InvaderGame = ({ modalDimensions }: InvaderGameProps): JSX.Element => {
   const [cellSize, setCellSize] = useState({ x: 1, y: 1 });
   const [playerPos, setPlayerPos] = useState({ x: 50, y: 90 });
   const [playerBulletPos, setPlayerBulletPos] = useState({ x: -100, y: -100 });
   const [playerBulletIsFiring, setPlayerBulletIsFiring] = useState(false);
   const [invaders, setInvaders] = useState<InvaderModel[][]>([]);
+  const [moveCondition, setMoveCondition] = useState<MoveCondition>({
+    id: 0,
+    dir: Direction.RIGHT,
+    change: false,
+  });
 
   const grid = { x: 100, y: 100 };
   const playerSize = {
@@ -47,7 +59,7 @@ const InvaderGame = ({ modalDimensions }: InvaderGameProps): JSX.Element => {
     for (let i = 0; i < 11; i++) {
       initialInvaders.push([] as InvaderModel[]);
       for (let j = 0; j < 4; j++) {
-        const id = j * 11 + i;
+        const id = (3 - j) * 11 + i;
         const x = 25 + i * 5;
         const y = 10 + j * 10;
         initialInvaders[i].push({ id, pos: { x, y }, size: { x: 5, y: 5 } });
@@ -55,6 +67,91 @@ const InvaderGame = ({ modalDimensions }: InvaderGameProps): JSX.Element => {
     }
     setInvaders(initialInvaders);
   }, []);
+
+  const moveInvaders = useCallback(() => {
+    let nextId = Infinity;
+    let nextDir = moveCondition.dir;
+    let change = moveCondition.change;
+    setInvaders((prevInvaders) => {
+      const minIds = prevInvaders.map((rows) => {
+        const ids = rows
+          .filter((invader) => moveCondition.id <= invader.id)
+          .map((invader) => invader.id);
+        return Math.min(...ids);
+      });
+      nextId = Math.min(...minIds);
+
+      if (nextId === Infinity) {
+        const allIds = prevInvaders.flatMap((rows) =>
+          rows.map((invader) => invader.id)
+        );
+        nextId = Math.min(...allIds);
+
+        if (moveCondition.dir === 'right_down') {
+          nextDir = Direction.LEFT;
+        } else if (moveCondition.dir === 'left_down') {
+          nextDir = Direction.RIGHT;
+        }
+        if (moveCondition.change) {
+          if (moveCondition.dir === 'right') {
+            nextDir = Direction.RIGHT_DOWN;
+          } else if (moveCondition.dir === 'left') {
+            nextDir = Direction.LEFT_DOWN;
+          }
+        }
+        change = false;
+      }
+
+      const updatedInvaders = prevInvaders.map((rows) =>
+        rows.map((invader) => {
+          if (invader.id === nextId) {
+            let dx = 0;
+            let dy = 0;
+            if (nextDir === 'right') {
+              dx = 2;
+            } else if (nextDir === 'left') {
+              dx -= 2;
+            } else {
+              dy = invader.size.y * 2;
+            }
+            if (
+              grid.x - invader.size.x <= invader.pos.x + invader.size.x + dx ||
+              invader.pos.x + dx <= invader.size.x
+            ) {
+              change = true;
+            }
+
+            return {
+              ...invader,
+              pos: {
+                x: invader.pos.x + dx,
+                y: invader.pos.y + dy,
+              },
+            };
+          } else {
+            return invader;
+          }
+        })
+      );
+
+      setMoveCondition((prevMoveCondition) => {
+        return {
+          ...prevMoveCondition,
+          id: nextId + 1,
+          dir: nextDir,
+          change,
+        };
+      });
+      return updatedInvaders;
+    });
+  }, [moveCondition]);
+
+  useEffect(() => {
+    const timer = setInterval(moveInvaders, 50);
+    return () => {
+      clearInterval(timer);
+    };
+  }, [moveInvaders]);
 
   const isCollision = ({
     rectPos,
--- src/enums.ts
+++ src/enums.ts
@@ -3,3 +3,10 @@ export enum Condition {
   Cloudy = 'cloudy',
   Rainy = 'rainy',
 }
+
+export enum Direction {
+  RIGHT = 'right',
+  LEFT = 'left',
+  RIGHT_DOWN = 'right_down',
+  LEFT_DOWN = 'left_down',
+}
